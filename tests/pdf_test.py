a59850c00692111242bca88eb7eb03aec0483461
f-terzuoli <francyterzu@gmail.com>
2019-10-28 20:00:27 +0100

    Improved tests on triangular pdf.

--- tests/pdf_test.py
+++ tests/pdf_test.py
@@ -17,81 +17,94 @@ from splrand.pdf import ProbabilityDensityFunction
 
 class TestPdf(unittest.TestCase):
 
-        """Unit test for the pdf module.
+    """Unit test for the pdf module.
+    """
+
+    def _test_triangular_base(self, xmin=0., xmax=1.0):
+        """Unit test with a triangular distribution.
         """
+        x = np.linspace(xmin, xmax, 101)
+        y = 2. / (xmax - xmin)**2 * (x - xmin)
+        pdf = ProbabilityDensityFunction(x, y)
 
-        def test_triangular(self):
-            """Unit test with a triangular distribution.
-            """
-            x = np.linspace(0., 1., 101)
-            y = 2. * x
-            pdf = ProbabilityDensityFunction(x, y)
-            a = np.array([0.2, 0.6])
-            print(pdf(a))
+        #Verify that the normalization is one.
+        norm = pdf.integral(xmin, xmax)
+        self.assertAlmostEqual(norm, 1.0)
 
-            plt.figure('pdf triangular')
-            plt.plot(x, pdf(x))
-            plt.xlabel('x')
-            plt.ylabel('pdf(x)')
+        #Verify that the pdf, evaluated on the input x-grid
+        delta = abs(pdf(x) - y)
+        self.assertTrue((delta < 1e-10).all())
 
-            plt.figure('cdf triangular')
-            plt.plot(x, pdf.cdf(x))
-            plt.xlabel('x')
-            plt.ylabel('cdf(x)')
+        plt.figure('pdf triangular')
+        plt.plot(x, pdf(x))
+        plt.xlabel('x')
+        plt.ylabel('pdf(x)')
 
-            plt.figure('ppf triangular')
-            q = np.linspace(0., 1., 250)
-            plt.plot(q, pdf.ppf(q))
-            plt.xlabel('q')
-            plt.ylabel('ppf(q)')
+        plt.figure('cdf triangular')
+        plt.plot(x, pdf.cdf(x))
+        plt.xlabel('x')
+        plt.ylabel('cdf(x)')
 
-            plt.figure('Sampling triangular')
-            rnd = pdf.rnd(1000000)
-            plt.hist(rnd, bins=200)
+        plt.figure('ppf triangular')
+        q = np.linspace(0., 1., 250)
+        plt.plot(q, pdf.ppf(q))
+        plt.xlabel('q')
+        plt.ylabel('ppf(q)')
 
+        plt.figure('Sampling triangular')
+        rnd = pdf.rnd(1000000)
+        plt.hist(rnd, bins=200)
 
-        def test_gauss(self, mu=0., sigma=1., support=10., num_points=500):
-            '''Unit test with a gaussian distribution
-            '''
-            from scipy.stats import norm
-            x = np.linspace(-support * sigma + mu, support * sigma + mu, num_points)
-            y = norm.pdf(x, mu, sigma)
-            pdf = ProbabilityDensityFunction(x, y)
-            plt.plot(x, y)
+    def test_triangular(self):
+        """
+        """
+        self._test_triangular_base(0., 1.)
+        self._test_triangular_base(0., 2.)
+        self._test_triangular_base(1., 2.)
 
-            plt.figure('cdf')
-            plt.plot(x, pdf.cdf(x))
-            plt.xlabel('x')
-            plt.ylabel('cdf(x)')
+    @unittest.skip('Temporary')
+    def test_gauss(self, mu=0., sigma=1., support=10., num_points=500):
+        '''Unit test with a gaussian distribution
+        '''
+        from scipy.stats import norm
+        x = np.linspace(-support * sigma + mu, support * sigma + mu, num_points)
+        y = norm.pdf(x, mu, sigma)
+        pdf = ProbabilityDensityFunction(x, y)
+        plt.plot(x, y)
 
-            plt.figure('ppf')
-            q = np.linspace(0., 1., 1000)
-            plt.plot(q, pdf.ppf(q))
-            plt.xlabel('q')
-            plt.ylabel('ppf(q)')
+        plt.figure('cdf')
+        plt.plot(x, pdf.cdf(x))
+        plt.xlabel('x')
+        plt.ylabel('cdf(x)')
 
-            plt.figure('Sampling')
-            rnd = pdf.rnd(10000000)
-            ydata, edges, _ = plt.hist(rnd, bins=200)
-            xdata = 0.5 * (edges[1:] + edges [:-1])
-            print(ydata.shape, edges.shape, xdata.shape)
+        plt.figure('ppf')
+        q = np.linspace(0., 1., 1000)
+        plt.plot(q, pdf.ppf(q))
+        plt.xlabel('q')
+        plt.ylabel('ppf(q)')
 
-            def f(x, C, mu, sigma):
-                return C * norm.pdf(x, mu, sigma)
+        plt.figure('Sampling')
+        rnd = pdf.rnd(10000000)
+        ydata, edges, _ = plt.hist(rnd, bins=200)
+        xdata = 0.5 * (edges[1:] + edges [:-1])
+        print(ydata.shape, edges.shape, xdata.shape)
 
-            popt, pcov = curve_fit(f, xdata, ydata)
-            print(popt)
-            print(np.sqrt(pcov.diagonal()))
-            _x = np.linspace(-10, 10, 500)
-            _y = f(_x, *popt)
-            plt.plot(_x, _y)
+        def f(x, C, mu, sigma):
+            return C * norm.pdf(x, mu, sigma)
 
-            mask = ydata > 0
-            chi2 = sum(((ydata[mask] - f(xdata[mask], *popt)) / np.sqrt(ydata[mask]))**2.)
-            nu = mask.sum() - 3
-            sigma = np.sqrt(2 * nu)
-            print(chi2, nu, sigma)
-            self.assertTrue(abs(chi2 - nu) < 5 * sigma)
+        popt, pcov = curve_fit(f, xdata, ydata)
+        print(popt)
+        print(np.sqrt(pcov.diagonal()))
+        _x = np.linspace(-10, 10, 500)
+        _y = f(_x, *popt)
+        plt.plot(_x, _y)
+
+        mask = ydata > 0
+        chi2 = sum(((ydata[mask] - f(xdata[mask], *popt)) / np.sqrt(ydata[mask]))**2.)
+        nu = mask.sum() - 3
+        sigma = np.sqrt(2 * nu)
+        print(chi2, nu, sigma)
+        self.assertTrue(abs(chi2 - nu) < 5 * sigma)
 
 
 
